52fdb751a1dca6b2b95dcbb82af09776e5f57938
tglima <thg.limadesousa@gmail.com>
2017-03-29 10:05:46 -0300

    Adicionado nos códigos fonte, os termos da licença GPL v3.

--- src/br/edu/tglima/StartApplication.java
+++ src/br/edu/tglima/StartApplication.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ *   License
+ *   Copyright 2017 Thiago Lima de Sousa <thg.limadesousa@gmail.com>
+ *  
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package br.edu.tglima;
 
 import br.edu.tglima.controller.*;
@@ -5,8 +22,8 @@ import br.edu.tglima.view.FramePrincipal;
 
 /**
  * @author tglima Thiago Lima de Sousa
- * @version 0.9.0
- * @build 20170328-1900
+ * @version 0.9.1
+ * @build 20170329-1000
  */
 
 public class StartApplication {
--- src/br/edu/tglima/view/DialogSobre.java
+++ src/br/edu/tglima/view/DialogSobre.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ *   License
+ *   Copyright 2017 Thiago Lima de Sousa <thg.limadesousa@gmail.com>
+ *  
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package br.edu.tglima.view;
 
 import java.awt.BorderLayout;
@@ -31,7 +48,7 @@ public class DialogSobre extends JDialog {
 	private JButton btnFechar = new JButton("Fechar");
 	
 	private final String msgSobre = "CRT - CÁLCULOS DE RESCISÃO DE TRABALHO\n\n"
-			+ "Versão 0.9.0 - Build 20170328-1900\n"
+			+ "Versão 0.9.1 - Build 20170329-1000\n"
 			+ "Copyright (c) 2017 Thiago Lima de Sousa\n\n"
 			+ "Imagens de Sebastian Rubio. (Licenciadas sob\n"
 			+ "GPLv3).\n\n"
